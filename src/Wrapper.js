ca152c7ab1872d82c1b06bc7f78b71de670c33aa
Samuel Imolorhe <samuelimolo4real@gmail.com>
2019-05-08 20:43:41 +0200

    Initial implementation.

---
+++ src/Wrapper.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import GraphiQLExplorer from 'graphiql-explorer';
+import { buildSchema, printSchema } from 'graphql';
+
+export class Wrapper extends Component {
+  static defaultProps = {
+    sdl: '',
+    query: '{ yo }',
+  };
+  state = {
+    schema: null
+  };
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    console.log('XXXX', 'mounted');
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (
+      newProps &&
+      newProps.sdl &&
+      (!this.props.sdl || newProps.sdl.length !== this.props.sdl.length)
+    ) {
+      console.log('XXXX', 'building schema', newProps, this.props);
+      this.setState({ schema: buildSchema(newProps.sdl) });
+    }
+  }
+
+  render() {
+    const { query } = this.props;
+    const noop = (...args) => console.log(...args);
+    const { schema } = this.state;
+
+    console.log(this.schema);
+
+    return (
+      <GraphiQLExplorer
+        schema={schema}
+        query={query}
+        onEdit={(query) => this.onEdit(query)}
+        explorerIsOpen={true}
+        onToggleExplorer={noop}
+      />
+    );
+  }
+
+  onEdit(query) {
+    const { ctx } = this.props;
+
+    ctx.setQuery(query);
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,110 @@
+import React, { createFactory } from 'react';
+//import ReactWebComponent from 'react-web-component';
+import ReactDOM from 'react-dom';
+// import { buildSchema } from 'graphql';
+import { Wrapper } from './Wrapper';
+
+import ReactiveElements, { registerReact } from 'reactive-elements';
+
+registerReact('x-qwasezio', Wrapper);
+// ReactWebComponent.create(<Wrapper />, 'x-qwasezio');
+
+// function defineElement(Component, elementName, observedAttributes) {
+//   class CustomElement extends HTMLElement {
+//     constructor(...args) {
+//       super(...args);
+//       console.log('XXXX', this);
+//       this.proxy = new Proxy(this, {
+//         get(target, prop) {
+//           console.log('XXXX::', prop);
+//           return target[prop];
+//         },
+//         set(target, prop, val) {
+//           console.log('XXXXXX', prop);
+//           if (this.observedAttributes.includes(prop)) {
+//             this.attributeChangedCallback(prop, target[prop], val);
+//             target[prop] = val;
+//           }
+//         }
+//       });
+//     }
+//     connectedCallback() {
+//       const customElement = this;
+//       const shadowRoot = this.attachShadow({mode: 'open'});
+//       const props = [...this.attributes].reduce((props, attribute) => ({...props, [attribute.name]: attribute.value }),
+//         {customElement, shadowRoot});
+
+//       const instance =(<Component {...(props)}/>);
+
+//       ReactDOM.render(instance, shadowRoot);
+
+//       this.instance = instance;
+//       this.props = props;
+//     }
+//     attributeChangedCallback(name, oldValue, newValue) {
+
+//       console.log('XXXXX', name, oldValue, newValue);
+//       const { shadowRoot, instance, props } = this;
+//       if(!instance) return;
+
+//       const newProps = {...(this.props), ...({[name]: newValue})};
+//       const newInstance =(<Component {...(newProps)}/>);
+
+//       ReactDOM.render(newInstance, shadowRoot);
+
+//       this.instance = newInstance;
+//       this.props = newProps;
+//     }
+//   }
+//   CustomElement.observedAttributes = observedAttributes;
+
+//   window.customElements.define(elementName, CustomElement);
+// }
+
+// defineElement(Wrapper, 'x-qwasezio', ['query', 'schema']);
+
+
+// class XSearch extends HTMLElement {
+//     // static get properties() {
+//     //     return {
+//     //         schema: Object
+//     //     };
+//     // }
+//   connectedCallback() {
+//     const mountPoint = document.createElement('span');
+//     this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
+
+//     const name = this.getAttribute('name');
+//       const url = 'https://www.google.com/search?q=' + encodeURIComponent(name);
+//       const schema = buildSchema(`
+//         type Query {
+//             hello: String
+//         }
+//         type Mutation
+//         type Subscription
+//       `);
+//       const query = `{ hello }`;
+//       const noop = () => {};
+//       this._upgradeProperty('schema');
+//       console.log(this.schema, this.attributes, this);
+//     ReactDOM.render(
+//         <GraphiQLExplorer
+//           schema={schema}
+//           query={query}
+//           onEdit={noop}
+//           explorerIsOpen={true}
+//           onToggleExplorer={noop}
+//         />
+//     , mountPoint);
+//   }
+//     _upgradeProperty(prop) {
+//       if (this.hasOwnProperty(prop)) {
+//           console.log('has prop', prop);
+//         let value = this[prop];
+//         delete this[prop];
+//         this[prop] = value;
+//       }
+//     }
+// }
+// customElements.define('x-qwasezio', XSearch);
+
